af211c49cb571f8aee893431e379fe06319ec1cd
alyadjey <likarenkovova1@gmail.com>
2022-07-01 16:44:53 +0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
 
+import { Component, OnDestroy } from '@angular/core';
+import { timer,Subscription } from 'rxjs';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'timer';
+ 
+export class AppComponent implements OnDestroy {
+  data: number = 0
+  isRunning:boolean = false
+  clickCount:number = 0
+  myObservable?:Subscription
+  start(){
+    this.isRunning = true
+    this.myObservable=timer(0,1000).subscribe(()=>{
+      this.data++
+    })
+    }
+  pause(){
+    this.isRunning = !this.isRunning
+    this.myObservable?.unsubscribe()
+    if(this.isRunning){
+      this.start()
+    }
+  }
+  stop(){
+    this.isRunning = false
+    this.reset()
+    this.myObservable?.unsubscribe()
+  }
+  reset(){
+    this.data = 0  
+  }
+  doubleClick(){
+   this.clickCount++
+    setTimeout(()=>{
+      if(this.clickCount === 2){
+        this.pause()
+      }
+      this.clickCount = 0
+    },500)
+  }
+  ngOnDestroy():void{
+    this.myObservable?.unsubscribe()
+  }
 }
